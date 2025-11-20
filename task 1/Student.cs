d5c766d6655191d225cc0a044f2126f7ea5d70d2
swikarstha <swikarstha5844@gmail.com>
2025-11-20 12:14:14 +0545

    task 1

---
+++ task 1/Student.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    internal class Student
+    {
+        public  String studentName = "Swikar";
+        public int studentRoll = 10;
+        public int studentAge = 19;
+
+
+        public static String studentAdress = "Damak";
+    }
+}
